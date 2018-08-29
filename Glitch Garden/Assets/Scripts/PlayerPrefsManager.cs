f35dbb86b223c7b1f0d40c475bc36b6e6a66ea51
DiversenSato <diversensato@gmail.com>
2018-08-29 16:27:14 +0200

    Glitch Garden added
    Project import

---
+++ Glitch Garden/Assets/Scripts/PlayerPrefsManager.cs
@@ -0,0 +1,67 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class PlayerPrefsManager : MonoBehaviour
+{
+    const string MASTER_VOLUME_KEY = "master_volume";
+    const string DIFF_KEY = "difficulty";
+    const string LEVEL_KEY = "level_unlocked_";
+    
+    public static void SetMasterVolume(float volume)
+    {
+        if (volume >= 0f && volume <= 1f)
+        {
+            PlayerPrefs.SetFloat(MASTER_VOLUME_KEY, volume);
+        } else
+        {
+            Debug.LogError("Master volume out of range");
+        }
+    }
+
+    public static float GetMasterVolume()
+    {
+        return PlayerPrefs.GetFloat(MASTER_VOLUME_KEY);
+    }
+
+    public static void UnlockLevel (int level)
+    {
+        if(level <= Application.levelCount - 1)
+        {
+            PlayerPrefs.SetInt(LEVEL_KEY + level.ToString(), 1); // Use 1 for true
+        } else
+        {
+            Debug.LogError("Trying to unlock a level that is not in build order");
+        }
+    }
+
+    public static bool IsLevelUnlocked(int level)
+    {
+        int levelValue = PlayerPrefs.GetInt(LEVEL_KEY + level.ToString());
+        bool isLevelUnlocked = (levelValue == 1);
+
+        if (level <= Application.levelCount - 1)
+        {
+            return isLevelUnlocked;
+        } else
+        {
+            Debug.LogError("Trying to query a level that is not in build order");
+            return false;
+        }
+    }
+
+    public static void SetDifficulty(int difficulty)
+    {
+        if (difficulty >= 1 && difficulty <= 3)
+        {
+            PlayerPrefs.SetInt(DIFF_KEY, difficulty);
+        } else
+        {
+            Debug.LogError("Difficulty is out of range");
+        }
+    }
+
+    public static float GetDifficulty()
+    {
+        return PlayerPrefs.GetInt(DIFF_KEY);
+    }
+}
